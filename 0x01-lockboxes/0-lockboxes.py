2e26999886e4815136f0e1f6d5616d41105b9fef
Thige254 <thigetj@gmail.com>
2024-10-03 16:26:02 +0000

    lockboxes task and readme

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,55 +1,39 @@
 #!/usr/bin/python3
-
-
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened.
-
-    Args:
-        boxes (list): A list of lists representing the boxes and keys.
-
-    Returns:
-        bool: True if all boxes can be opened, else False.
-    """
-    if not boxes:
-        return False
-
-    # Initialize a set to keep track of visited boxes
-    visited = set()
-
-    # Initialize a queue with the first box
-    queue = [0]
-
-    # Iterate over the queue
-    while queue:
-        # Pop the first box from the queue
-        current_box = queue.pop(0)
-
-        # If the current box has already been visited, skip it
-        if current_box in visited:
-            continue
-
-        # Add the current box to the visited set
-        visited.add(current_box)
-
-        # Add all keys in the current box to the queue
+    """Determines if all boxes can be opened."""
+    # Number of boxes
+    n = len(boxes)
+    
+    # A set to keep track of boxes we can open (start with box 0)
+    unlocked = set([0])
+    
+    # A stack to track boxes we need to check (start with box 0)
+    to_check = [0]
+    
+    # Loop while there are boxes to check
+    while to_check:
+        # Pop a box from the stack
+        current_box = to_check.pop()
+        
+        # Go through all keys in the current box
         for key in boxes[current_box]:
-            if key < len(boxes):
-                queue.append(key)
+            # If the key corresponds to a box we haven't unlocked yet
+            if key not in unlocked and key < n:
+                # Unlock the box and add it to the set of unlocked boxes
+                unlocked.add(key)
+                # Add the newly unlocked box to the stack for further exploration
+                to_check.append(key)
+    
+    # Return True if we've unlocked all boxes, otherwise False
+    return len(unlocked) == n
 
-    # Check if all boxes have been visited
-    return len(visited) == len(boxes)
-
-
-# Sample test cases
 if __name__ == "__main__":
-    boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))  # Output: True
+    # Test cases
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
 
-    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))  # Output: True
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
 
-    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))  # Output: False
-
-# Additional test cases provided by the checker
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
