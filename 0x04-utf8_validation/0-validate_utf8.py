406dcadcde1abc63ee5a241bbcfbc989cb349d27
Thige254 <thigetj@gmail.com>
2024-10-15 04:02:02 +0000

    correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,13 +3,14 @@
 This module provides a function for validating UTF-8 encoding.
 """
 
+
 def validUTF8(data):
     """
     Determine if a given dataset represents a valid UTF-8 encoding.
-    
+
     Parameters:
     data (list of int): A list of integers representing the bytes of data.
-    
+
     Returns:
     bool: True if the data is a valid UTF-8 encoding, else False.
     """
@@ -31,11 +32,11 @@ def validUTF8(data):
             while mask & byte:
                 number_of_bytes += 1
                 mask = mask >> 1
-            
+
             # If the byte does not indicate a multi-byte character, continue
             if number_of_bytes == 0:
                 continue
-            
+
             # UTF-8 can be at most 4 bytes long
             if number_of_bytes == 1 or number_of_bytes > 4:
                 return False
